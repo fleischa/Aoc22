a7320b86894fe6021aa6b0cfe6d82b4f7caf4ea4
Fabian Fleischer <ffleischa@gmail.com>
2022-12-06 14:03:28 +0100

    156

--- Aoc22_06_Golf/Program.cs
+++ Aoc22_06_Golf/Program.cs
@@ -1,1 +1,1 @@
-﻿var s=File.OpenText("i").ReadToEnd();void f(int m)=>Console.WriteLine(Enumerable.Range(0,s.Length-m+1).First(i=>s[i..(i+m)].Distinct().Count()==m)+m);f(4);f(14);
+﻿var s=File.OpenText("i").ReadToEnd();void f(int m)=>Console.WriteLine(Enumerable.Range(m,s.Length).First(i=>s[(i-m)..i].Distinct().Count()==m));f(4);f(14);
