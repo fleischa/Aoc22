fd6195bc265b44a2d86daa3bd2a3931a7b836f37
Fabian Fleischer <ffleischa@gmail.com>
2022-12-06 13:23:18 +0100

    .

---
+++ Aoc22_06/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Aoc22_06;
+
+internal class Program
+{
+	private static async Task Main(string[] args)
+	{
+		using StreamReader reader = File.OpenText("input.txt");
+		string signal = await reader.ReadToEndAsync();
+
+		Console.WriteLine($"start-of-packet: {Program.FindMarker(signal, 4)}");
+		Console.WriteLine($"start-of-message: {Program.FindMarker(signal, 14)}");
+	}
+
+	private static int FindMarker(string signal, int markerLength)
+	{
+		return Enumerable.Range(0, signal.Length - markerLength + 1)
+			       .First(i => signal.Substring(i, markerLength).Distinct().Count() == markerLength) +
+		       markerLength;
+	}
+}
