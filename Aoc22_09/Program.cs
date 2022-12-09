182a72776084b72a3d7b3ed4db0a049c6898caf6
Fabian Fleischer <fabian.fleischer@crosberg.de>
2022-12-09 14:13:26 +0100

    Aoc22_09

---
+++ Aoc22_09/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Aoc22_09;
+
+using AocCommon;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        RopeSpace ropeSpace = new(10);
+
+        await new SingleLineProcessor<RopeSpace>((line, rs) =>
+        {
+            string[] tokens = line.Split(' ');
+
+            (int x, int y) move;
+
+            switch (tokens[0])
+            {
+                case "L":
+                    move = new(-1, 0);
+                    break;
+                case "U":
+                    move = new(0, 1);
+                    break;
+                case "R":
+                    move = new(1, 0);
+                    break;
+                case "D":
+                    move = new(0, -1);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            for (int i = 0; i < int.Parse(tokens[1]); i++)
+            {
+                rs!.MoveHead(move);
+            }
+        }).Process(ropeSpace);
+
+        Console.WriteLine($"unique tail locations: {ropeSpace.UniqueTailLocations.Count}");
+    }
+}
+
---
+++ Aoc22_09/RopeSpace.cs
@@ -0,0 +1,46 @@
+﻿namespace Aoc22_09;
+
+internal class RopeSpace
+{
+    public RopeSpace(int ropeLength)
+    {
+        this.Rope = Enumerable.Repeat<(int x, int y)>(new(0, 0), ropeLength).ToArray();
+    }
+
+    public HashSet<(int x, int y)> UniqueTailLocations { get; init; } = new() { new(0, 0) };
+
+    public (int X, int Y)[] Rope { get; init; }
+
+    public void MoveHead((int x, int y) move)
+    {
+        this.Rope[0] = new(this.Rope[0].X + move.x, this.Rope[0].Y + move.y);
+
+        for (int i = 0; i < this.Rope.Length - 1; i++)
+        {
+            (int X, int Y) h = this.Rope[i];
+            (int X, int Y) t = this.Rope[i + 1];
+
+            int dX = h.X - t.X;
+            int dY = h.Y - t.Y;
+
+            if (Math.Abs(dX) < 2 && Math.Abs(dY) < 2)
+            {
+                // h and t are adjacent
+                continue;
+            }
+
+            this.Rope[i + 1] = new(t.X + Math.Sign(dX), t.Y + Math.Sign(dY));
+        }
+
+        this.UniqueTailLocations.Add(this.Rope.Last());
+    }
+}
+
+
+
+
+
+
+
+
+
